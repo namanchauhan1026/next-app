5074a926d37a722712e4ec92fe546d2ece126540
namanchauhan1026 <namanchaauhanvv@gmail.com>
2025-08-06 23:15:13 +0530

    tsc

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,23 @@
-import { client } from "@repo/db/client";
+// import { client } from "@repo/db/client";
 
 
 export default async function Home() {
-  const user = await client.user.findMany();
-  const userMap = user.map((user) => ({
-    username: user.username,
-    password: user.password,
-  }));
+  // const user = await client.user.findMany();
+  // const userMap = user.map((user) => ({
+  //   username: user.username,
+  //   password: user.password,
+  // }));
   
   return (
     <div>
       <h1>This is the list of users in the database</h1>
-      {userMap.map((user: any) => (
+      {/* {userMap.map((user: any) => (
         <div key={user.username}>
           <h1>auto deploy</h1>
           <h2>user name is {user.username} and password is {user.password}</h2>
         </div>
-      ))}
+      ))} */}
+      <p>Database connection temporarily disabled for build testing</p>
     </div>
   );
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer, dev }) => {
+    // Prevent webpack from scanning system directories
+    config.watchOptions = {
+      ignored: [
+        '**/node_modules',
+        '**/.git',
+        '**/dist',
+        '**/build',
+        '**/.next',
+        '**/C:/Users/**',
+        '**/C:/ProgramData/**',
+        '**/C:/Windows/**',
+        '**/C:/Program Files/**',
+        '**/C:/Users/naman/Application Data/**',
+        '**/C:/Users/naman/AppData/**',
+        '**/C:/Users/naman/Cookies/**'
+      ]
+    };
+    
+    // Disable file watching during build
+    if (!dev) {
+      config.watchOptions = {
+        ignored: ['**/*']
+      };
+    }
+    
+    return config;
+  },
+  experimental: {
+    // Disable webpack build worker to prevent file system scanning
+    webpackBuildWorker: false
+  }
+};
 
 export default nextConfig;
